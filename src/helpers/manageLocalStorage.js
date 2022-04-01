d35f5a6d55614a1108333fe5c97a7437d49a2fed
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-04-01 17:26:53 +0530

    localStorage helper

---
+++ src/helpers/manageLocalStorage.js
@@ -0,0 +1,29 @@
+const saveToLocalStorage = (str, data) => {
+  try {
+    localStorage.setItem(str, JSON.stringify(data));
+    console.log("Item stored with name =>", str);
+  } catch (err) {
+    console.log("Data cant be stored with name =>", str, err);
+  }
+};
+
+const getFromLocalStorage = (str) => {
+  try {
+    const data = JSON.parse(localStorage.getItem(str));
+    return data;
+  } catch (err) {
+    console.log("Your data cant be fethced");
+    return null;
+  }
+};
+
+const clearLocalStorage = () => {
+  try {
+    localStorage.clear();
+    console.log("Local storage cleared");
+  } catch (err) {
+    console.log("Local storage can't be cleared");
+  }
+};
+
+export { saveToLocalStorage, getFromLocalStorage, clearLocalStorage };
