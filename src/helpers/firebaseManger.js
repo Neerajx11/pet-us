37c7c9e040bfa9921172c1385a039621015e5626
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-04-02 19:45:58 +0530

    doggo add/update/delete fucn added

--- src/helpers/firebaseManger.js
+++ src/helpers/firebaseManger.js
@@ -1,19 +1,21 @@
-import { async } from "@firebase/util";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import {
   addDoc,
   collection,
+  deleteDoc,
   doc,
   getDocs,
   query,
   serverTimestamp,
   setDoc,
+  updateDoc,
   where,
 } from "firebase/firestore";
 import { auth, db, provider } from "../firebase";
 
 import createUserObj from "./createUserObj";
 
+// =============== AUTHENTICATION ================
 export const userSignIn = async () => {
   try {
     const res = await signInWithPopup(auth, provider);
@@ -37,6 +39,7 @@ export const userSignOut = async () => {
   }
 };
 
+// ================== DB ===================
 export const saveUserToDB = async ({ uid, name, email, photoURL }) => {
   try {
     await setDoc(doc(db, "users", uid), {
@@ -51,31 +54,82 @@ export const saveUserToDB = async ({ uid, name, email, photoURL }) => {
   }
 };
 
-export const addDoggoDetail = async () => {
+/**
+ * @param {string} str : Data to fetch. options[myData, myDoggo, users, doggo]
+ * @param {string} uid : req for myData and myDoggo data
+ * @link https://firebase.google.com/docs/firestore/manage-data/add-data#update-data
+ */
+export const getData = async (str, uid = null) => {
+  const list = [];
+  let querySnapshot;
+
+  //query according to requirement
+  switch (str) {
+    case "myData":
+      // return current user details
+      querySnapshot = await getDocs(collection(db, `users/${uid}`));
+      break;
+    case "users":
+      // return all the users
+      querySnapshot = await getDocs(collection(db, "users"));
+      break;
+    case "doggo":
+      // return all the dogs
+      querySnapshot = await getDocs(collection(db, "doggo"));
+      break;
+    case "myDoggo":
+      // return your dogs
+      querySnapshot = await getDocs(
+        query(collection(db, "doggo"), where("owner", "==", uid))
+      );
+      break;
+    default:
+      return list;
+  }
+
+  querySnapshot.forEach((doc) => list.push({ ...doc.data(), uid: doc.id }));
+  return list;
+};
+
+//============== DOGGO CUD =====================
+/**
+ * @param {object} data : data to add
+ * @param {string} id : generate this from uuid v4()
+ * @description upload image first if avilable then add doggo
+ */
+export const addDoggoDetail = async (data, id) => {
   try {
-    const res = await addDoc(collection(db), {
-      //data
+    const res = await addDoc(collection(db, "doggo", id), {
+      ...data,
       timeStamp: serverTimestamp(),
     });
+    console.log(res);
   } catch (err) {
     console.log(err);
   }
 };
 
-export const getAllUser = async () => {
-  const list = [];
-  const querySnapshot = await getDocs(collection(db, "users"));
-  querySnapshot.forEach((doc) => list.push({ ...doc.data(), uid: doc.id }));
-  return list;
+/**
+ * @param {number} uid : doggo unique id
+ * @param {object} data : data to update
+ * @link https://firebase.google.com/docs/firestore/manage-data/add-data#update-data
+ */
+export const updateDoggo = async (uid, data) => {
+  try {
+    await updateDoc(doc(db, "doggo", uid), data);
+    console.log("Doggo updated");
+  } catch (err) {
+    console.log("Doggo cant be updated err=>", err);
+  }
 };
 
-export const getYourDoggo = async (uid = null) => {
-  const list = [];
-
-  const querySnapshot = await getDocs(
-    query(collection(db, "doggo"), where("owner", "==", uid))
-  );
-  querySnapshot.forEach((doc) => list.push({ ...doc.data(), uid: doc.id }));
-
-  return list;
+/**
+ * @param {number} uid : doggo unique id
+ */
+export const deleteDoggo = async (uid) => {
+  try {
+    await deleteDoc(doc(db), "doggo", uid);
+  } catch (err) {
+    console.log(err);
+  }
 };
