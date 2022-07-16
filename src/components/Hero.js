88867d16dd7310c3796863316c73a6abc47b3fe7
Neeraj Maurya <mauryaneeraj11@gmail.com>
2022-07-16 13:24:51 +0530

    ui fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,25 +7,27 @@ import { Facebook, GitHub, Instagram } from "react-feather";
 const Hero = () => {
   return (
     <div className="relative flex items-center shadow-md">
-      <div className="absolute flex w-1 h-48 mr-16 top-44 left-4 bg-primary"></div>
+      <div className="absolute hidden w-1 h-48 mr-16 md:flex top-44 left-4 bg-primary"></div>
       {/* Main section */}
-      <div className="flex flex-col justify-between w-full mx-10 md:mx-24 lg:space-x-10 lg:flex-row xl:mx-32">
+      <div className="flex flex-col justify-between w-full mx-0 sm:mx-10 md:mx-24 lg:space-x-10 lg:flex-row xl:mx-32">
         <div className="py-12 mt-6 text-center md:mt-12 lg:text-left">
           <p className="pb-12 text-5xl font-bold md:text-6xl">
             <span className="text-bgprimary">You can </span>{" "}
             <span className="text-primary">Help!</span>
           </p>
-          <p className="mx-auto mb-12 leading-8 tracking-wide w-96 md:w-34">
+          <p className="mx-auto mb-12 leading-8 tracking-wide w-96 ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vero
             illum ratione molestias? Facilis consequatur asperiores commodi hic
             necessitatibus.
           </p>
-          <NavLink to="/explore">
-            <Button
-              className="mx-auto font-bold tracking-wide w-80 lg:w-full"
-              text="Get your pet today"
-            />
-          </NavLink>
+          <div className="flex justify-center lg:justify-start">
+            <NavLink to="/explore">
+              <Button
+                className="mx-auto font-bold tracking-wide w-80 lg:w-full"
+                text="Get your pet today"
+              />
+            </NavLink>
+          </div>
         </div>
         <div className="flex items-center mx-auto min-w-32 lg:items-end">
           <img
